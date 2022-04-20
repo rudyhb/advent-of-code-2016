72b3f172da63768c815965308f6b874d77e12fa3
Rudy <rudyhasbun@gmail.com>
2022-04-20 14:02:09 -0600

    day 6

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ mod day2_bathroom_security;
 mod day3_squares_with_three_sides;
 mod day4_security_through_obscurity;
 mod day5_game_of_chess;
+mod day6_signals_and_noise;
 
 fn main() {
     let _timer = Timer::start(|elapsed| println!("main took {} ms.", elapsed.as_millis()));
     let day: usize = if let Some(arg1) = std::env::args().nth(1) {
         arg1.parse().expect("argument should be an integer")
     } else {
-        5
+        6
     };
     println!("running day {}\n", day);
     match day {
@@ -20,6 +21,7 @@ fn main() {
         3 => day3_squares_with_three_sides::run(),
         4 => day4_security_through_obscurity::run(),
         5 => day5_game_of_chess::run(),
+        6 => day6_signals_and_noise::run(),
         _ => panic!("day {} not found", day)
     }
 }
