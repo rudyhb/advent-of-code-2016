07dba1511b263e5194560e508f0bb5c706265f87
Rudy <rudyhasbun@gmail.com>
2022-04-19 20:40:34 -0600

    day 3

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ use utils::timer::Timer;
 
 mod day1_no_time_for_taxicab;
 mod day2_bathroom_security;
+mod day3_squares_with_three_sides;
 
 fn main() {
     let _timer = Timer::start(|elapsed| println!("main took {} ms.", elapsed.as_millis()));
     let day: usize = if let Some(arg1) = std::env::args().nth(1) {
         arg1.parse().expect("argument should be an integer")
     } else {
-        2
+        3
     };
     println!("running day {}\n", day);
     match day {
         1 => day1_no_time_for_taxicab::run(),
         2 => day2_bathroom_security::run(),
+        3 => day3_squares_with_three_sides::run(),
         _ => panic!("day {} not found", day)
     }
 }
